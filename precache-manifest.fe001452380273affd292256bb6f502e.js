527ed216ca92897a8c4abee664f3c40a22e8ec11
cijiugechu <cijiugechu@gmail.com>
2020-03-01 19:05:58 +0800

    Revert "Initialize project using Create React App"
    This reverts commit c002c3404d289961aad0ebaa83ff332d10b10903.

---
+++ precache-manifest.fe001452380273affd292256bb6f502e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c310dcb9ab821dfd257ed9a4b74b698",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21fa7451568393a29b15",
+    "url": "/static/css/main.0828e645.chunk.css"
+  },
+  {
+    "revision": "a2d5d275e3d347c632ea",
+    "url": "/static/js/2.792a2790.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.792a2790.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21fa7451568393a29b15",
+    "url": "/static/js/main.16783b38.chunk.js"
+  },
+  {
+    "revision": "f838f563f2d9d5a7b997",
+    "url": "/static/js/runtime-main.38f4ca1e.js"
+  }
+]);
